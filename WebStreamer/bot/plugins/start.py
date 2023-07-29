57a02bcbd0a6f86155dcbc71543ea6caa05b1504
vivekaprasad <96980843+vivekaprasad@users.noreply.github.com>
2023-07-29 08:50:30 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -11,8 +11,7 @@ from WebStreamer.bot import StreamBot
 async def start(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
-            "You are not in the allowed list of users who can use me. \
-            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
+            "You are not in the allowed list of users who can use me..",
             disable_web_page_preview=True, quote=True
         )
     await m.reply(
